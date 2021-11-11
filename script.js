bc23b0d97401714c489be0e53c9cd50453a2515f
uGame <lord.rebrad@gmail.com>
2021-11-12 02:15:48 +0800

    #4

--- script.js
+++ script.js
@@ -3,34 +3,47 @@ class NavigationButton {
     buttonOn;
     buttonOff;
     popup;
-    isActive = false;
+    shadow;
+    body
+    showed = true;
 
     constructor() {
         this.buttonOff = document.getElementById('navigation-button__off');
         this.buttonOn = document.getElementById('navigation-button__on');
         this.popup = document.getElementById('mobile-popup');
+        this.shadow = document.getElementById('shadow');
+        this.body = document.getElementsByTagName('body')[0];
+        this.shadow.addEventListener('click', () => {
+            this.hide()
+        })
+
         this.toggle();
     }
 
     toggle() {
-        this.isActive = !this.isActive
-        if (this.isActive) {
-            this.enable();
+        if (this.showed) {
+            this.hide()
             return;
         }
-        this.disable()
+        this.show();
     }
 
-    enable() {
-        this.buttonOn.classList.remove('hide')
-        this.buttonOff.classList.add('hide')
-        this.popup.classList.add('hide')
-    }
-
-    disable() {
+    show() {
+        this.showed = true;
         this.buttonOn.classList.add('hide')
+        this.shadow.classList.remove('hide')
         this.buttonOff.classList.remove('hide')
-        this.popup.classList.remove('hide')
+        this.popup.classList.add('mobile-popup__show')
+        this.body.classList.add('overflow-hide')
+    }
+
+    hide() {
+        this.showed = false;
+        this.buttonOn.classList.remove('hide')
+        this.shadow.classList.add('hide')
+        this.buttonOff.classList.add('hide')
+        this.popup.classList.remove('mobile-popup__show')
+        this.body.classList.remove('overflow-hide')
     }
 }
 
